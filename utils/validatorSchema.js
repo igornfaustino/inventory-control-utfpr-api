c1c191788a41c3477256f59c416dfc0389510f90
Igor Neves Faustino <igornfaustino@gmail.com>
2018-04-24 20:29:14 -0300

    Create a validator Schemas Files

---
+++ utils/validatorSchema.js
@@ -0,0 +1,54 @@
+const Joi = require('joi');
+
+const PriceSchema = {
+    _id: Joi.any(),
+    type: Joi.string().required(),
+    priceRef: Joi.string().required(),
+    value: Joi.number().required(),
+};
+
+const SupplierSchema = {
+    _id: Joi.any(),
+    name: Joi.string().required(),
+    cnpj: Joi.string().required(),
+    phone: Joi.string().required(),
+    address: Joi.object().keys({
+        _id: Joi.any(),
+        number: Joi.number(),
+        street: Joi.string(),
+        city: Joi.string(),
+        state: Joi.string(),
+        country: Joi.string(),
+    }).required()
+};
+
+const ItemsRequisitionSchema = {
+    _id: Joi.any(),
+    siorg: Joi.string(),
+    __v: Joi.any(),
+    description: Joi.string().regex(/^(=?.*[A-Za-z\d$@$!%*?&])$/).required(),
+    justification: Joi.string().regex(/^(=?.*[A-Za-z\d$@$!%*?&])$/).required(),
+    priceJustification: Joi.string().regex(/^(=?.*[A-Za-z\d$@$!%*?&])$/),
+    qtd: Joi.number().required(),
+    prices: Joi.array().items(Joi.object().keys(PriceSchema)),
+    itemSupplier: Joi.object().keys(SupplierSchema)
+};
+
+
+const PurchaseRequisitionSchema = {
+    _id: Joi.any(),
+    management: Joi.string().required(),
+    requisitionDate: Joi.date().required(),
+    UGR: Joi.string().required(),
+    __v: Joi.any(),
+    sector: Joi.string().required(),
+    requester: Joi.string().required(),
+    requisitionItems: Joi.array().items(Joi.object().keys(ItemsRequisitionSchema)).required()
+}
+
+module.exports = {
+    PriceSchema,
+    SupplierSchema,
+    ItemsRequisitionSchema,
+    PurchaseRequisitionSchema
+}
